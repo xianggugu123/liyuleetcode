422faf607e3ab95441a0ed928dddcfa44984fd42
liyu <liyu@dudu.com>
2023-11-11 16:31:45 +0800

    数组操作

---
+++ 数组删除重复元素26.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int slow = 1;
+        for (int fast = 1; fast < nums.size(); fast++)
+        {
+            if (nums[fast] != nums[fast - 1])
+            {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+        }
+        cout << slow << endl;
+        return slow;
+    }
+};
+int main()
+{
+    vector<int>nums{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    Solution a;
+    a.removeDuplicates(nums);
+	return 0;
+}
