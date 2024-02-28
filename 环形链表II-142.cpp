98d5bdc85f56df8b2f5bf4f49274760478e4e2ca
xianggugu <1351737248@qq.com>
2024-02-28 19:37:27 +0800

    环形链表

---
+++ 环形链表II-142.cpp
@@ -0,0 +1,49 @@
+//给定一个链表的头节点  head ，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+
+//如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 - 1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+
+//不允许修改 链表。
+#include <iostream>
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int val) :val(val), next(nullptr) {}
+};
+class Solution {
+public:
+    ListNode* detectCycle(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast != nullptr && fast->next != nullptr && fast->next->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (fast == slow) {
+
+                ListNode* index1 = head;
+                ListNode* index2 = fast;
+                while (index1 != index2)
+                {
+                    index1 = index1->next;
+                    index2 = index2->next;
+
+                }
+                return index1;
+            }
+
+        }
+        return NULL;
+    }
+};
+int main()
+{
+	return 0;
+}
