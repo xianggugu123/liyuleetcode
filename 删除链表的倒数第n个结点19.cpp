b82cf2ba184712ae9342f4c9b44b3264e0ff4c8f
xianggugu <1351737248@qq.com>
2024-02-28 09:42:10 +0800

    2.28

---
+++ 删除链表的倒数第n个结点19.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+#include <list>
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        ListNode* fast = dummyHead;
+        ListNode* slow = dummyHead;
+        ++n;
+        while (n-- && fast != nullptr) {
+            fast = fast->next;
+
+        }
+        while (fast) {
+
+            fast = fast->next;
+            slow = slow->next;
+        }
+        ListNode* temp = slow->next;
+        slow->next = slow->next->next;
+        delete(temp);
+        temp = nullptr;
+        return dummyHead->next;
+    }
+    void print(ListNode* head)
+    {
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        ListNode* cur = dummyHead;
+        while (cur->next)
+        {
+            cout << cur->next->val << " ";
+            cur = cur->next;
+        }
+        cout << endl;
+
+    }
+};
+int main()
+{
+    Solution a;
+    list<int> p = { 1,2,3,4,5 };
+
+    ListNode* head = nullptr;
+    ListNode* cur = nullptr;
+    for (int val : p) {
+
+        if (head == nullptr)
+        {
+            head = new ListNode(val);
+            cur = head;
+        }
+        else
+        {
+            cur->next = new ListNode(val);
+            cur = cur->next;
+        }
+    }
+    a.print(head);
+    ListNode* heiheiihei = a.removeNthFromEnd(head,5);
+    a.print(heiheiihei);
+    
+    return 0;
+}
