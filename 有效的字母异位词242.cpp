50b7b7e0b2dffb900d3bf8f7061ae6a7acd588df
xianggugu <1351737248@qq.com>
2024-02-28 19:47:31 +0800

    哈希算法

---
+++ 有效的字母异位词242.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+#include <string>
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int hash[26] = { 0 };
+        for (int i = 0; i < s.size(); i++) {
+            hash[s[i] - 'a']++;
+        }
+        for (int i = 0; i < t.size(); i++) {
+            hash[t[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (hash[i] != 0) {
+                cout << "000" << endl;
+                return false;
+            }
+        }
+        cout << "666" << endl;
+        return true;
+    }
+};
+int main()
+{
+    Solution a;
+    string s = "anagram";
+    string t = "nagaram";
+    a.isAnagram(s, t);
+	return 0;
+}
