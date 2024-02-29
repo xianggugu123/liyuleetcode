53ce1b8142965d9e6b90e16fcc3d580905ddea29
xianggugu <1351737248@qq.com>
2024-02-29 11:17:16 +0800

    3

---
+++ 两个数组的交集349-数组哈希.cpp
@@ -0,0 +1,29 @@
+//给定两个数组 nums1 和 nums2 ，返回 它们的交集 。输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> result_set;
+        int hash[1005] = { 0 };
+        for (int num : nums1) {
+            hash[num] = 1;
+        }
+        for (int num : nums2) {
+            if (hash[num] == 1) {
+                result_set.insert(num);
+            }
+        }
+        return vector<int>(result_set.begin(), result_set.end());
+    }
+};
+int main()
+{
+    vector<int>nums1 = { 1,2,2,1 };
+    vector<int>nums2 = { 2,2 };
+    Solution a;
+    a.intersection(nums1, nums2);
+    return 0;
+}
