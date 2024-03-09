9c972ac19667c5ef186c1230cc05b31119dbbc4d
xianggugu <1351737248@qq.com>
2024-03-09 22:44:21 +0800

    kmp解法

---
+++ 重复的字符串459-kmp解法.cpp
@@ -0,0 +1,24 @@
+/*给定一个非空的字符串 s ，检查是否可以通过由它的一个子串重复多次构成*/
+
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string t = s + s;
+        t.erase(t.begin());
+        t.erase(t.end() - 1);
+        if (t.find(s) != string::npos) {
+            cout << true;
+            return true;
+        }
+        return false;
+    }
+};
+int main()
+{
+    string s = "abab";
+    Solution a;
+    a.repeatedSubstringPattern(s);
+    return 0;
+}
