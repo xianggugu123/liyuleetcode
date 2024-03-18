f4de98c91228606afea84d34ea4c87375868980d
xianggugu <1351737248@qq.com>
2024-03-18 20:23:56 +0800

    二叉树右视图-层序遍历解决

---
+++ 199. 二叉树的右视图.cpp
@@ -0,0 +1,47 @@
+//给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>result;
+        queue<TreeNode*>que;
+        if (root != nullptr) {
+            que.push(root);
+        }
+        while (!que.empty()) {
+
+            int size = que.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = que.front();
+                que.pop();
+                if (i == size - 1) {
+                    result.push_back(node->val);
+                }
+                if (node->left) {
+                    que.push(node->left);
+                }
+                if (node->right) {
+                    que.push(node->right);
+                }
+            }
+        }
+        return result;
+    }
+};
+//int main()
+//{
+//	return 0;
+//}
